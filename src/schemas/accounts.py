dad9198737e5da6a896f06615b663638e22f641a
Andriy Shafran <shafran.andriy@gmail.com>
2026-05-25 15:52:41 +0300

    feat: Added code to src/schemas/accounts.py

--- src/schemas/accounts.py
+++ src/schemas/accounts.py
@@ -3,4 +3,37 @@ from pydantic import BaseModel, EmailStr, field_validator
 from database import accounts_validators
 
 
-# Write your code here
+class FilmBase(BaseModel):
+    title: str
+    genre: str
+    price: float
+
+class FilmCreate(FilmBase):
+    pass
+
+class FilmUpdate(FilmBase):
+    pass
+
+class FilmRead(FilmBase):
+    id: int
+
+    class Config:
+        from_attributes = True
+
+class UserBase(BaseModel):
+    email: EmailStr
+
+class UserCreate(UserBase):
+    password: str
+    role: str = "user"
+
+class UserRead(UserBase):
+    id: int
+    role: str
+
+    class Config:
+        from_attributes = True
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
