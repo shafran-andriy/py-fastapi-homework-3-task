4a7b7d903644a64da6661c898f032d62b4c82c15
Andriy Shafran <shafran.andriy@gmail.com>
2026-05-25 16:25:55 +0300

    fix: Fixed code in src/schemas/accounts.py

--- src/schemas/accounts.py
+++ src/schemas/accounts.py
@@ -3,37 +3,66 @@ from pydantic import BaseModel, EmailStr, field_validator
 from database import accounts_validators
 
 
-class FilmBase(BaseModel):
-    title: str
-    genre: str
-    price: float
-
-class FilmCreate(FilmBase):
-    pass
-
-class FilmUpdate(FilmBase):
-    pass
-
-class FilmRead(FilmBase):
-    id: int
-
-    class Config:
-        from_attributes = True
-
-class UserBase(BaseModel):
+class EmailBaseSchema(BaseModel):
     email: EmailStr
 
-class UserCreate(UserBase):
+    @field_validator("email")
+    @classmethod
+    def validate_email(cls, value: EmailStr) -> str:
+        return accounts_validators.validate_email(str(value).lower())
+
+
+class PasswordBaseSchema(BaseModel):
     password: str
-    role: str = "user"
 
-class UserRead(UserBase):
+    @field_validator("password")
+    @classmethod
+    def validate_password(cls, value: str) -> str:
+        return accounts_validators.validate_password_strength(value)
+
+
+class UserRegistrationRequestSchema(EmailBaseSchema, PasswordBaseSchema):
+    pass
+
+
+class UserRegistrationResponseSchema(BaseModel):
     id: int
-    role: str
+    email: EmailStr
 
-    class Config:
-        from_attributes = True
+    model_config = {
+        "from_attributes": True
+    }
 
-class Token(BaseModel):
+
+class UserActivationRequestSchema(EmailBaseSchema):
+    token: str
+
+
+class MessageResponseSchema(BaseModel):
+    message: str
+
+
+class PasswordResetRequestSchema(EmailBaseSchema):
+    pass
+
+
+class PasswordResetCompleteRequestSchema(EmailBaseSchema, PasswordBaseSchema):
+    token: str
+
+
+class UserLoginRequestSchema(EmailBaseSchema):
+    password: str
+
+
+class UserLoginResponseSchema(BaseModel):
     access_token: str
-    token_type: str
+    refresh_token: str
+    token_type: str = "bearer"
+
+
+class TokenRefreshRequestSchema(BaseModel):
+    refresh_token: str
+
+
+class TokenRefreshResponseSchema(BaseModel):
+    access_token: str
